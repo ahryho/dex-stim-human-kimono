c48e64a63e4420d58d04c63810d99ff7940af39f
Anastasiia Hryhorzhevska <ahryhorzhevska@slurmgate.psych.mpg.de>
2021-04-16 14:02:09 +0200

    sh script to run on cluster

--- 02_run_kimono/02_run_kimono_dex.sh
+++ 02_run_kimono/02_run_kimono_dex.sh
@@ -5,8 +5,9 @@ src_dir=/home/ahryhorzhevska/mpip/code/dex-stim-human-kimono
 # module load R
 
 partition=pe
+node=4
 memory=200G
-job_name=kimono
+job_name=kimono_dex
 
-sbatch --job-name=$job_name --part=$partition --mem=$memory \
+sbatch --job-name=$job_name --part=$partition --nodelist=$partition$node --mem=$memory \
 --wrap="Rscript --vanilla $src_dir/02_run_kimono/02_run_kimono_dex.R"
