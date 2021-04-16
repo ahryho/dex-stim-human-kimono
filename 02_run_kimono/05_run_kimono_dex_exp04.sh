6260dd3fdbd0a3ef27e25b064d9c86ff50023899
Anastasiia Hryhorzhevska <ahryhorzhevska@slurmgate.psych.mpg.de>
2021-04-16 23:44:01 +0200

    shell script ficed

--- 02_run_kimono/05_run_kimono_dex_exp04.sh
+++ 02_run_kimono/05_run_kimono_dex_exp04.sh
@@ -10,4 +10,4 @@ memory=200G
 job_name=kimono_veh_exp04
 
 sbatch --job-name=$job_name --part=$partition --nodelist=$partition$node --mem=$memory \
---wrap="Rscript --vanilla $src_dir/02_run_kimono/05_run_kimono_dexh_exp04.R"
+--wrap="Rscript --vanilla $src_dir/02_run_kimono/05_run_kimono_dex_exp04.R"
