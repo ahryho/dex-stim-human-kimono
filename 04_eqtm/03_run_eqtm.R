6a8a407a596d143385e7e7ada6c59831dc649ca9
anastasiiainna <anastasiia_hry@PCR183.fritz.box>
2021-04-24 00:20:53 +0200

    script for eqtm calculation

--- 04_eqtm/03_run_eqtm.R
+++ 04_eqtm/03_run_eqtm.R
@@ -51,24 +51,24 @@ run_matrix_eqtl <- function(SNP_file, expression_file, cov_file, outfile_cis, ou
   # Now we need to set up the snp and gene expression data in the special format required by the MatrixEQTL package
   # 2.1 SNP Data
   snps = SlicedData$new()
-  snps$fileDelimiter <- ";"     # the TAB character
-  snps$fileOmitCharacters <- "NA" # denote missing values;
-  snps$fileSkipRows <- 1          # one row of column labels
-  snps$fileSkipColumns <- 1       # one column of row labels
-  snps$fileSliceSize <- 100000     # read file in pieces of 100,000 rows
+  snps$fileDelimiter = ";"     # the TAB character
+  snps$fileOmitCharacters = "NA" # denote missing values;
+  snps$fileSkipRows = 1          # one row of column labels
+  snps$fileSkipColumns = 1       # one column of row labels
+  snps$fileSliceSize = 100000     # read file in pieces of 100,000 rows
   snps$LoadFile(SNP_file)
   
   # 2.2 Gene Expression Data
-  gene <- SlicedData$new()
-  gene$fileDelimiter <- ";"      # the TAB character
-  gene$fileOmitCharacters <- "NA" # denote missing values;
-  gene$fileSkipRows <- 1          # one row of column labels
-  gene$fileSkipColumns <- 1      # one column of row labels
-  gene$fileSliceSize <- 2000      # read file in pieces of 2,000 rows
+  gene = SlicedData$new()
+  gene$fileDelimiter = ";"      # the TAB character
+  gene$fileOmitCharacters = "NA" # denote missing values;
+  gene$fileSkipRows = 1          # one row of column labels
+  gene$fileSkipColumns = 1      # one column of row labels
+  gene$fileSliceSize = 2000      # read file in pieces of 2,000 rows
   gene$LoadFile(expression_file)
   
   # 2.3 Covariates
-  cvrt <- SlicedData$new()
+  cvrt = SlicedData$new()
   cvrt$fileDelimiter = ";"      # the TAB character
   cvrt$fileOmitCharacters = "NA" # denote missing values;
   cvrt$fileSkipRows = 1          # one row of column labels
