0c48e5cf6026925fedc0f84d5542194a35b7ac4b
Anastasiia Hryhorzhevska <ahryhorzhevska@slurmgate.psych.mpg.de>
2021-04-25 21:32:39 +0200

    parameters changed

--- 02_run_kimono/01_run_kimono.sh
+++ 02_run_kimono/01_run_kimono.sh
@@ -5,14 +5,16 @@ src_dir=/home/ahryhorzhevska/mpip/code/dex-stim-human-kimono
 # module load R
 
 partition=pe
-node=4
-memory=200G
+node=5
+memory=500G
 job_name=kimono_
 
 treatment="dex"
 sbatch --job-name=$job_name$treatment --part=$partition --nodelist=$partition$node --mem=$memory \
 	--wrap="Rscript --vanilla $src_dir/02_run_kimono/01_run_kimono.R $treatment"
 
+node=7
+memory=500G
 treatment="veh"
 sbatch --job-name=$job_name$treatment --part=$partition --nodelist=$partition$node --mem=$memory \
 	--wrap="Rscript --vanilla $src_dir/02_run_kimono/01_run_kimono.R $treatment"
